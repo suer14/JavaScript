a64d629cd47cbebb613470b577a79a0aca3f3a00
suer14 <nmonteiro.art@gmail.com>
2021-04-16 12:02:38 +0100

    Update source.js

--- source.js
+++ source.js
@@ -5,3 +5,26 @@ let writOutput = function(text)
 
 writeOutput("Done!");
 
+let truncateString = function(str,num)
+{
+    if(str.length <= num)
+    {
+        return str;
+    }
+
+    return str.slice(0,num)+'...';
+
+}
+document.write(truncateString("Hoje está um dia feioso",22));
+
+document.write(truncateString("Hoje está um dia feioso",10));
+
+function titleCase(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join(' '); 
+ }
+ 
+ document.write(titleCase("hoje está a chover demasiado"));
